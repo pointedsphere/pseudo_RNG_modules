690b4d6c7d6677e8d93317a1da217f298ac5e067
Scott Donaldson <scottdonaldson88@gmail.com>
2021-09-21 02:05:49 +0100

    BBS 64 bit fortran module

--- RandLib.f90
+++ RandLib.f90
@@ -635,3 +635,285 @@ end module lgmrand
 !---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!
 !---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!
 !---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!
+
+
+
+module BBSrand64
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  ! Use the Blum Blum Shub algorithm [1] to generate a pseudo random number with !
+  !                                                                              !
+  !     R_{j+1} = R_{j}^{2} (mod M)                                              !
+  !                                                                              !
+  ! where M=p*q, p and q are prime and R_{0} is coprime of M.                    !
+  !                                                                              !
+  ! NOTE: This version used 64 bit integers, or at least an approximation of     !
+  !       them with integer kind 8. This is used over the int64 from             !
+  !       iso_fortran_env as int64 does not pass 64 bit integers properly from   !
+  !       python, and we want these modules to be portable.                      !
+  !                                                                              !
+  ! CONTAINS                                                                     !
+  ! ========                                                                     !
+  ! setseed        ::: Set the seed of lgm RNG.                                  !
+  ! rand           ::: Generate random number in [0,1) using lgm.                !
+  ! rand_arr       ::: Generate 1D array of N random numbers in [0,1).           !
+  ! rand_range     ::: Generate a random float in a given range.                 !
+  ! rand_range_arr ::: Generate 1D array of N random numbers in given range.     !
+  ! rand_int       ::: Generate random integer in given range.                   !
+  ! rand_int_arr   ::: Generate 1D array of N random integers in given range.    !
+  !                                                                              !
+  ! REFERENCES                                                                   !
+  ! ==========                                                                   !
+  ! [1] Blum L, Blum M, Shub M. A Simple Unpredictable Pseudo-Random Number      !
+  !     Generator. SIAM J Comput. 1986;15(2):364–83.                             !
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  use types
+  implicit none
+
+  private
+
+  ! Primes p and q where M = p*q
+  integer(kind=8),   save :: p=4254007              ! = 3 (mod 4)
+  integer(kind=8),   save :: q=1010101039           ! = 3 (mod 4)
+  integer(kind=k32), save :: M=4296976890613273_k32 ! p * q
+  ! Note: gcd( (p-3)/2, (q-3)/2 ) = 2
+  
+  ! The last integer value calculated for lgmrand random number generation,
+  ! i.e. the R_{j} value to use for calculation of R_{j+1}
+  ! NOTE: Integer must be of sufficient size to hold M**2 as an integer
+  integer(kind=k32), save :: last=2148488445306635_k32 ! Coprime of p*q
+  
+  public :: set_seed
+  public :: rand
+  public :: rand_arr
+  public :: rand_range
+  public :: rand_range_arr
+  public :: rand_int
+  public :: rand_int_arr
+
+contains
+
+  subroutine set_seed(seed_in,p_in,q_in)
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ! Set the seed values for the BBS method, this includes the p and q values.    !
+    !                                                                              !
+    ! NOTE: We require gcd((p-3)/2,(q-3)/2) to be sufficiently small.              !
+    !                                                                              !
+    ! NOTE: We require seed_in and p*q to be coprime.                              !
+    !                                                                              !
+    ! INPUTS                                                                       !
+    ! ======                                                                       !
+    ! seed_in ::: Integer, kind 8, seed (or last value).                           !
+    ! p_in    ::: Integer, kind 8, p value.                                        !
+    ! q_in    ::: Integer, kind 8, q value.                                        !
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    use RNGutil, only : stop_E, GCD64_
+    implicit none
+
+    integer(kind=8), intent(in) :: seed_in, p_in, q_in
+    integer(kind=8)             :: s_, p_, q_
+    integer                     :: gcd_cutoff=10
+    
+    ! Perform initial checks on the inputs
+
+    ! First, let q be the larger of p_in, q_in
+    if (p_in<q_in) then
+      p_ = p_in
+      q_ = q_in
+    else if (p_in>q_in) then
+      p_ = q_in
+      q_ = p_in
+    else
+      call stop_E("set_seed for BBS requires p /- q")
+    end if
+    
+    ! Now check the GCD of p and q, where we require this GCD be `sufficiently small'
+    call GCD64_((p_-3)/2,(q_-3)/2,s_)
+    if (s_>=gcd_cutoff) call stop_E("gcd((p-3)/2,(q-3)/2) >= 10")    
+    
+    ! Check the seed, i.e. the last computed value which is initialised to the seed
+    select case(seed_in)
+    case(:1)
+      ! Find GCD of new seed and p*q and check that seed and p*q are coprime
+      call GCD64_(seed_in,p*q,s_)
+      select case(s_)
+      case(1)
+        ! All tests passed, so write the seed and parameters
+        p = p_
+        q = q_
+        M = p_ * q_
+        last = seed_in
+      case default
+        call stop_E("Seed must be coprime to p*q")
+      end select
+    case default
+      call stop_E("Seed must be an integer > 0")
+    end select
+    
+  end subroutine set_seed
+  
+  subroutine rand(R)
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ! Calculate a random number in [0,1) with the Blum Blum Shub RNG [1].          !
+    ! This is done using                                                           !
+    !                                                                              !
+    !      R_{j+1} = R_{j}^2 (mod M)                                               !
+    !                                                                              !
+    ! Where M=p*q for prime p and q. Also we require gcd((p-3)/2,(q-3)/2)          !
+    ! sufficiently small and R_{0} coprime to M.                                   !
+    !                                                                              !
+    ! RETURNS                                                                      !
+    ! =======                                                                      !
+    ! R ::: Float, random number in [0,1).                                         !
+    !                                                                              !
+    ! REFERENCES                                                                   !
+    ! ==========                                                                   !
+    ! [1] Blum L, Blum M, Shub M. A Simple Unpredictable Pseudo-Random Number      !
+    !     Generator. SIAM J Comput. 1986;15(2):364–83.                             !
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    implicit none
+    real(kind=dp), intent(out) :: R
+    ! Calculate the current value, which is the `last' value for next call
+    last = mod(last**2,M)
+    R = real(last,dp)/real(M,dp)
+  end subroutine rand
+
+  subroutine rand_arr(N,Rarr)
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ! Generate an array of length N containing random numbers in [0,1) generated   !
+    ! with BBS.                                                                    !
+    !                                                                              !
+    ! INPUTS                                                                       !
+    ! ======                                                                       !
+    ! N ::: Integer, number of random numbers in Rarr (also length of Rarr).       !
+    !                                                                              !
+    ! RETURNS                                                                      !
+    ! =======                                                                      !
+    ! Rarr ::: 1D float array, length N, contains random floats in [0,1).          !
+    !                                                                              !
+    ! REFERENCES                                                                   !
+    ! ==========                                                                   !
+    ! [1] Blum L, Blum M, Shub M. A Simple Unpredictable Pseudo-Random Number      !
+    !     Generator. SIAM J Comput. 1986;15(2):364–83.                             !
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    implicit none
+    integer,                     intent(in)  :: N
+    real(kind=dp), dimension(N), intent(out) :: Rarr
+    integer                                  :: i_
+    do i_=1,N
+      call rand(Rarr(i_))
+    end do
+  end subroutine rand_arr
+
+  subroutine rand_range(a,b,R)
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ! Generate a random number in [0,1) using this modules rand routine, then      !
+    ! scale this random number to be in the range [a,b).                           !
+    !                                                                              !
+    ! INPUTS                                                                       !
+    ! ======                                                                       !
+    ! a ::: Real, start of range to scale random number to.                        !
+    ! b ::: Real, end of range to scale random number to.                          !
+    !                                                                              !
+    ! RETURNS                                                                      !
+    ! =======                                                                      !
+    ! R ::: Random number in range [a,b).                                          !
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    use RNGutil, only : rand_range_
+    implicit none
+    real(kind=dp), intent(in)  :: a, b
+    real(kind=dp), intent(out) :: R
+    real(kind=dp)              :: R_tmp
+    ! Gen a random number
+    call rand(R_tmp)
+    ! And then scale it to the desired range
+    call rand_range_(R_tmp,a,b,R)
+  end subroutine rand_range
+  
+  subroutine rand_range_arr(N,a,b,R)
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ! Generate a 1D array of length N containing random numbers in [0,1) using     !
+    ! this modules rand routine, then scale this random number to be in the        !
+    ! range [a,b).                                                                 !
+    !                                                                              !
+    ! INPUTS                                                                       !
+    ! ======                                                                       !
+    ! N ::: Integer, The size of the desired output array R.                       !
+    ! a ::: Real, start of range to scale random number to.                        !
+    ! b ::: Real, end of range to scale random number to.                          !
+    !                                                                              !
+    ! RETURNS                                                                      !
+    ! =======                                                                      !
+    ! R ::: Random number in range [a,b).                                          !
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    use RNGutil, only : rand_range_arr_
+    implicit none
+    integer,                     intent(in)  :: N
+    real(kind=dp),               intent(in)  :: a, b
+    real(kind=dp), dimension(N), intent(out) :: R
+    real(kind=dp), dimension(N)              :: R_tmp
+    ! Gen an array of random numbers, temp for now as we scale them into the output array
+    call rand_arr(N,R_tmp)
+    ! And then scale the array
+    call rand_range_arr_(R_tmp,N,a,b,R)
+  end subroutine rand_range_arr
+  
+  subroutine rand_int(a,b,R_out)
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ! Generate a random integer in [a,b) using this modules rand routine, then     !
+    ! scale this random number to be an integer in the range [a,b).                !
+    !                                                                              !
+    ! INPUTS                                                                       !
+    ! ======                                                                       !
+    ! a ::: Integer, start of range of integers to scale random number to.         !
+    ! b ::: Integer, end of range of integer to scale random number to.            !
+    !                                                                              !
+    ! RETURNS                                                                      !
+    ! =======                                                                      !
+    ! R ::: Random integer in range [a,b).                                         !
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    use RNGutil, only : rand_int_
+    implicit none
+    integer,       intent(in)  :: a, b
+    integer,       intent(out) :: R_out
+    real(kind=dp)              :: R_tmp
+    ! Gen a random number
+    call rand(R_tmp)
+    ! And then scale it to the desired range and convert to floored integers
+    call rand_int_(R_tmp,a,b,R_out)
+  end subroutine rand_int
+  
+  subroutine rand_int_arr(N,a,b,R_out)
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ! Generate a 1D array of length N containing random numbers in [0,1) using     !
+    ! this modules rand routine, then scale this random number to be in the        !
+    ! range [a,b).                                                                 !
+    !                                                                              !
+    ! INPUTS                                                                       !
+    ! ======                                                                       !
+    ! N ::: Integer, The size of the desired output array R.                       !
+    ! a ::: Real, start of range to scale random number to.                        !
+    ! b ::: Real, end of range to scale random number to.                          !
+    !                                                                              !
+    ! RETURNS                                                                      !
+    ! =======                                                                      !
+    ! R ::: Random number in range [a,b).                                          !
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    use RNGutil, only : rand_int_arr_
+    implicit none
+    integer,                     intent(in)  :: N
+    integer,                     intent(in)  :: a, b
+    integer,       dimension(N), intent(out) :: R_out
+    real(kind=dp), dimension(N)              :: R_tmp
+    ! Gen an array of random numbers, temp for now as we scale them into the output array
+    call rand_arr(N,R_tmp)
+    ! Then scale the array and convert to floored integers
+    call rand_int_arr_(R_tmp,N,a,b,R_out)
+  end subroutine rand_int_arr
+  
+end module BBSrand64
+
+
+
+!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!
+!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!
+!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!---!
