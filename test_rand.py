2b42c5a07fb8111d495e1628e69bf2f06afa139c
Scott Donaldson <scottdonaldson88@gmail.com>
2021-09-23 03:17:41 +0100

    Include new method in python

--- test_rand.py
+++ test_rand.py
@@ -32,8 +32,6 @@ class rand_test:
         # Analysis of the random number array
         self.mean      = self.rand_mean()
         self.mean_diff = abs(self.mean-0.5)
-        self.chi_sq    = self.chi_squared()
-        self.chi_sq_p  = self.chi_sq[1]
         self.pi_diff   = abs( self.pi_est() - np.pi )
         
     def plot_hist(self):
@@ -45,18 +43,6 @@ class rand_test:
     def rand_mean(self):
         # Find the mean of the random number array
         return np.sum(self.R)/self.N
-        
-    def chi_squared(self):
-        # Calculate the chi squared error
-
-        # Bin the data from the array of random numbers
-        binned = np.bincount(np.digitize(self.R, bins=np.linspace(0+(1/self.numbins),1,self.numbins)))
-        
-        # The expected height of each bin
-        e = self.N/self.numbins
-
-        # Return the chi square with the scipy state module function
-        return stats.chisquare(binned,f_exp=np.full(shape=self.numbins,fill_value=e))
         
     def pi_est(self):
         # Calculate pi using the random numbers in the self.R array
@@ -75,24 +61,29 @@ class rand_test:
     
 if __name__ == "__main__":
 
-    N = 100000    # Number of random numbers to generate
+    N = 10000    # Number of random numbers to generate
     numbins = 100 # Number of bins for graphing/chi-squared testing
+
+    # a = rngutil.bitarr2int32_([1,0,1,0,1,0,1,0,0,0,1,0,0,0,0,1,0,1,0,0,1,0,1,1,0,0,0,1,1,0,1,0],32)
+
+    # print(a)
     
     # Calcualte the random numbers
     F    = rand_test(N,numbins,frand.rand,frand.rand_arr)
     lgm  = rand_test(N,numbins,lgmrand.rand,lgmrand.rand_arr)
     bbs  = rand_test(N,numbins,bbsrand64.rand,bbsrand64.rand_arr)
     lfsr = rand_test(N,numbins,lfsr258.rand,lfsr258.rand_arr)
+    ca   = rand_test(N,numbins,ca32.rand,ca32.rand_arr)
 
     fig, ax = plt.subplots(2, 3, figsize=(12,9))
     fig.tight_layout(pad=5,h_pad=4,w_pad=4)
-
+    
     # Histograms
     histax = plt.subplot2grid((2, 3), (0, 0), colspan=3)
     histax.hist(
-        [F.R, lgm.R, bbs.R, lfsr.R],
+        [F.R, lgm.R, bbs.R, lfsr.R, ca.R],
         numbins,
-        label=['Fortran','LGM','BBS','LFSR258'],
+        label=['Fortran','LGM','BBS','LFSR258','CA32'],
         histtype='step',
         stacked=False,
         fill=False)
@@ -105,8 +96,8 @@ if __name__ == "__main__":
     # Mean barchart
     meanax = plt.subplot2grid((2, 3), (1, 0), colspan=1)
     meanax.bar(
-        ['Fortran','LGM','BBS','LFSR258'],
-        [F.mean_diff, lgm.mean_diff, bbs.mean_diff, lfsr.mean_diff],
+        ['Fortran','LGM','BBS','LFSR258','CA32'],
+        [F.mean_diff, lgm.mean_diff, bbs.mean_diff, lfsr.mean_diff, ca.mean_diff],
         )
     meanax.grid(True)
     meanax.set_ylabel('|Mean of Rand Arr - 0.5|')
@@ -114,8 +105,8 @@ if __name__ == "__main__":
     # Pi difference barchart
     piax = plt.subplot2grid((2, 3), (1, 1), colspan=1)
     piax.bar(
-        ['Fortran','LGM','BBS','LFSR258'],
-        [F.pi_diff, lgm.pi_diff, bbs.pi_diff, lfsr.mean_diff],
+        ['Fortran','LGM','BBS','LFSR258','CA32'],
+        [F.pi_diff, lgm.pi_diff, bbs.pi_diff, lfsr.pi_diff, ca.pi_diff],
         )
     piax.grid(True)
     piax.set_ylabel('|Estimate of pi - pi|')
@@ -123,8 +114,8 @@ if __name__ == "__main__":
     # Calculation time barchart
     timeax = plt.subplot2grid((2, 3), (1, 2), colspan=1)
     timeax.bar(
-        ['Fortran','LGM','BBS','LFSR258'],
-        [F.ave_time, lgm.ave_time, bbs.ave_time, lfsr.mean_diff],
+        ['Fortran','LGM','BBS','LFSR258','CA32'],
+        [F.ave_time, lgm.ave_time, bbs.ave_time, lfsr.ave_time, ca.ave_time],
         )
     timeax.set_yscale('log')
     timeax.grid(True)
